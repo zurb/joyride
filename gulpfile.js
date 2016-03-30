f3d23ea167f3011522c1f278947ba34843b1904d
Marius Olbertz <marius.olbertz@gmail.com>
2016-03-30 15:53:58 +0200

    Finalized visual testing.
    Added browser-sync.
    Implemented watching of files + browser reloading.
    Moved demo files to tests/visual.
    Visual tests now run default gulp task, which will build, watch and serve files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
   runSequence = require('run-sequence'),
   rimraf = require('rimraf').sync,
   requireDir = require('require-dir'),
-  config = require('./gulp/config');
+  browser = require('browser-sync'),
+  config = require('./gulp/config'),
+  port = process.env.SERVER_PORT || 3001;
 
 requireDir('./gulp');
 
@@ -12,7 +14,7 @@ requireDir('./gulp');
  * Gulp task default.
  * Calls build:all.
  */
-gulp.task('default', ['build:all']);
+gulp.task('default', ['serve', 'watch']);
 
 /**
  * Gulp task build:all.
@@ -33,6 +35,26 @@ gulp.task('build:foundation', ['js:foundation', 'css:foundation']);
  * Calls js:standalone and css:standalone.
  */
 gulp.task('build:standalone', ['js:standalone', 'css:standalone']);
+
+/**
+ * Gulp task watch.
+ * Watch files for changes and reloads the browser.
+ */
+gulp.task('watch', function() {
+  gulp.watch('js/**/*', [[['js:foundation'], 'js:standalone'], browser.reload]);
+  gulp.watch('scss/**/*', [[['css:foundation'], 'css:standalone'], browser.reload]);
+  gulp.watch('test/visual/*.html', [browser.reload]);
+});
+
+
+/**
+ * Gulp task serve.
+ * Starts a BrowerSync instance.
+ */
+gulp.task('serve', ['build:all'], function(){
+  browser.init({server: './test/visual', port: port});
+});
+
 
 /**
  * Gulp task clean.
